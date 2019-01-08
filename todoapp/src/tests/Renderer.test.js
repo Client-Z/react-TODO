40581a35aa8354a793198ce09f9018a79765d0ed
admin <admin@admins-MacBook-Pro.local>
2019-01-08 13:54:36 +0200

    a little update

--- todoapp/src/tests/Renderer.test.js
+++ todoapp/src/tests/Renderer.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
-import Navbar from '../components/Navbar'
 import Adapter from 'enzyme-adapter-react-16'
+import Navbar from '../components/Navbar'
 
 Enzyme.configure({ adapter: new Adapter() });
 
